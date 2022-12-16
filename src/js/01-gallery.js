59f22ad0f29a5bff69f1ec925531189e48e6d339
AnttonnIT <97734056+AnttonnIT@users.noreply.github.com>
2022-12-16 18:38:33 +0200

    up

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
 
-console.log(galleryItems);
+import "simplelightbox/dist/simple-lightbox.min.css";
+const galleryEl = document.querySelector(".gallery");
+
+const galleryStr = galleryItems
+  .map(
+    ({
+      description,
+      original,
+      preview,
+    }) => `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`
+  )
+  .join("");
+
+galleryEl.innerHTML = galleryStr;
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
