83d0fea9d36702cb15a79b76b4a740c0e2eef7f8
AnttonnIT <anttonnit@gmail.com>
2022-12-20 18:48:09 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,22 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
-});
+const CURRENT_TIME = 'videoplayer-current-time';
+const iframeEl = document.querySelector('iframe');
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+const player = new Player(iframeEl);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(CURRENT_TIME, seconds);
+}
+
+setCurrentTime();
+
+function setCurrentTime() {
+  if (!localStorage.getItem(CURRENT_TIME)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+}
